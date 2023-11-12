211b4da41a6f46df3ecb0a33d0def6c1ea6daf15
dmitry <stanko.11@mail.ru>
2023-11-12 12:49:31 +0700

    find best model add

--- src/IntersectionDetector.py
+++ src/IntersectionDetector.py
@@ -8,6 +8,7 @@ import matplotlib.patches as mpatches
 from PIL import Image, ImageDraw
 from shapely.geometry import Polygon
 from ultralytics import YOLO
+from ensemble_boxes import weighted_boxes_fusion
 
 
 class IntersectionDetector():
@@ -158,14 +159,24 @@ class IntersectionDetector():
         # plt.show()
         return image
 
-    # def compute_metrics(boxes, confidences, weights, iou_thr=0.5, skip_box_thr=0.001):
-    #     """
-    #     Computes WBF metrics for bounding boxes and confidences
-    #     """
-    #     labels = [[0 for _ in conf] for conf in confidences]
-    #     res = weighted_boxes_fusion(boxes, confidences, labels,
-    #                                 weights=weights, iou_thr=iou_thr, skip_box_thr=skip_box_thr)
-    #     return [res[0].tolist(), res[1].tolist()]
+    def compute_metrics(boxes, confidences, weights, iou_thr=0.5, skip_box_thr=0.001):
+        """
+        Computes WBF metrics for bounding boxes and confidences
+
+        :param boxes:       List of lists of bounding boxes prediction for every model in ensemble
+        :type boxes:        list
+        :param confidences: Confidences for every prediction
+        :type confidences:  list
+        :param weights:     Weights for every model in ensemble
+        :type weights:      list
+        :param iou_thr:     IoU threshold
+        :type  iou_thr:     float
+        """
+        labels = [[0 for _ in conf] for conf in confidences]
+        res = weighted_boxes_fusion(boxes, confidences, labels,
+                                    weights=weights, iou_thr=iou_thr, skip_box_thr=skip_box_thr)
+        return [res[0].tolist(), res[1].tolist()]
+
 
     def __make_prediction(self) -> None:
         """Метод создания прогноза"""
